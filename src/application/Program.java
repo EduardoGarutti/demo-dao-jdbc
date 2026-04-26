4bb990917ae2839532d56253f7d96101eb51b379
Eduardo <eduardo.vinicius.garutti@gmail.com>
2026-04-26 15:23:20 -0300

    Adicionando o método FindByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -17,6 +17,14 @@ public class Program {
 		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
+		
+		System.out.println("\n==== Teste 2: seller FindByDepartment ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerdao.findByDepartment(department);
+		
+		for (Seller a : list) {
+			System.out.println(a);
+		}
 	}
 
 }
