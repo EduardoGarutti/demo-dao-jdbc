058bcff4fe2695879d0b67e7b6c8c83ff59edda9
Eduardo <eduardo.vinicius.garutti@gmail.com>
2026-04-26 13:41:11 -0300

    Criando classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1, "Books");
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dp);
 		
-		System.out.println(dp);
+		System.out.println(seller);
 	}
 
 }
