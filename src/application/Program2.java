8030efdf64f929842d0313479aa349a477a1116f
Eduardo <eduardo.vinicius.garutti@gmail.com>
2026-04-26 16:23:12 -0300

    Adicionando método para inserção

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,11 +2,24 @@ package application;
 
 import java.util.Scanner;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		
+		System.out.println("==== Teste 1 : Método de Inserir ====");
+		Department dep = new Department(null, "Musica");
+		
+		DepartmentDao depDao = DaoFactory.creatDepartmentDao();
+		
+		depDao.insert(dep);
+		
+		System.out.println("Inserção feita com sucesso! ");
 		sc.close();
 	}
 
