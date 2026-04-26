793c57ded36f55e2a592e010a86130ae5731dc1c
Eduardo <eduardo.vinicius.garutti@gmail.com>
2026-04-26 16:36:43 -0300

    Adicionando método de Update

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -12,14 +12,25 @@ public class Program2 {
 		Scanner sc = new Scanner(System.in);
 		
 		
-		System.out.println("==== Teste 1 : Método de Inserir ====");
+		System.out.println("==== Teste 1: Método de Inserir ====");
 		Department dep = new Department(null, "Musica");
 		
 		DepartmentDao depDao = DaoFactory.creatDepartmentDao();
 		
 		depDao.insert(dep);
 		
-		System.out.println("Inserção feita com sucesso! ");
+		System.out.println("Inserção feita com sucesso! Novo id: "+ dep.getId());
+		
+		System.out.println("==== Teste 2: Método de Update ====");
+		
+		System.out.println("Digite um novo nome para uma categoria: ");
+		dep.setName(sc.nextLine());
+		System.out.println("Digite o id para fazer o update: ");
+		dep.setId(sc.nextInt());
+		
+		depDao.update(dep);
+		
+		System.out.println("Update realizado com sucesso! ");
 		sc.close();
 	}
 
