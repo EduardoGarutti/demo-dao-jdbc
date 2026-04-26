e6042e6427d67e80ce4286fd15c88c4cb56c1e2e
Eduardo <eduardo.vinicius.garutti@gmail.com>
2026-04-26 14:18:17 -0300

    Implementando o método FindById

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, dp);
-		
 		SellerDao sellerdao = DaoFactory.creatSellerDao();
+		
+		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
 	}
